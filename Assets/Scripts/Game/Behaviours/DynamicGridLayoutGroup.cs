df62afcdcdcd445fc79d7fce8c96cfca253d9c4b
Bruno Silva Freire <brunorbsf@gmail.com>
2024-03-15 18:39:09 -0300

    Final responsive layout

---
+++ Assets/Scripts/Game/Behaviours/DynamicGridLayoutGroup.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.Behaviours
+{
+    public class DynamicGridLayoutGroup : GridLayoutGroup
+    {
+        [SerializeField] protected int cellsPerLine = 1;
+        [SerializeField] protected float aspectRatio = 1;
+
+        public override void SetLayoutVertical()
+        {
+            float width = (this.GetComponent<RectTransform>()).rect.width;
+            float useableWidth = width - this.padding.horizontal - (this.cellsPerLine - 1) * this.spacing.x;
+            float cellWidth = useableWidth / cellsPerLine;
+            this.cellSize = new Vector2(cellWidth, cellWidth * this.aspectRatio);
+            base.SetLayoutVertical();
+        }
+    }
+}
